654613884713de7c24763da5933cc7ac679eb533
Yashraj Mandloi <er.mandloiyashraj@gmail.com>
2025-07-16 00:08:04 +0530

    add feature : Jwt Token validator

---
+++ src/main/java/com/codepenguin/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.codepenguin.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "ajljejaijfLJEIOWJkjeflakejfkjaekjaf2332r&7203ljafiose";
+    public static final String JWT_HEADER = "Authorization";
+}
